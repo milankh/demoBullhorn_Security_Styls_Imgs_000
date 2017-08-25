b22e72368e48ebef502e40e4adab15858326dfa9
RVSW317027DT <RVSW317027DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-25 15:33:28 -0400

    Message

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -2,15 +2,13 @@ package com.example.demo;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
+@Table(name="userData")
 public class User {
 
     @Id
